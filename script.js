b3e8c44c1f42bea550828e5a7ea3edec8f9a0c33
Keshav Madhav <keshav2552003@gmail.com>
2024-06-22 09:45:34 +0530

    Created basic circle and circumfrence rotation

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+let time = 0;
+let radius = 100;
+
 /// Resize Canvas
 window.addEventListener('resize', resizeCanvas);
 function resizeCanvas() {
@@ -15,6 +18,30 @@ resizeCanvas();
 /// Game Loop
 const gameLoop = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  time -= 0.03;
+
+  ctx.strokeStyle = 'white'; 
+  ctx.lineWidth = 1;
+  ctx.beginPath();
+  ctx.arc(200, 200, radius, 0, Math.PI * 2);
+  ctx.stroke(); 
+  ctx.closePath();
+
+  let x = 200 + radius * Math.cos(time);
+  let y = 200 + radius * Math.sin(time);
+
+  ctx.beginPath();
+  ctx.moveTo(200, 200);
+  ctx.lineTo(x, y);
+  ctx.stroke()
+
+  ctx.fillStyle = 'white';
+  ctx.beginPath();
+  ctx.arc(x, y, 4, 0, Math.PI * 2);
+  ctx.fill(); // Fill the shape
+  ctx.closePath();
+
 
   requestAnimationFrame(gameLoop);
 };
